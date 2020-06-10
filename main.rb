9bfbfd7f86d532599489e8ab6894a68c90e0e060
AyupovFN <kapterka@bk.ru>
2020-06-10 17:46:55 +0300

    Add function parse from radio

--- main.rb
+++ main.rb
@@ -13,8 +13,19 @@ class Song
   end
 
   def to_s
-    "#{artist} #{song}"
+    "#{artist} - #{song}"
   end
+end
+
+URL = 'https://maximum.ru/'
+html = open(URL)
+doc = Oga.parse_html(html)
+
+doc.css('.track-animation').each do |m|
+  artist = m.at_css('.artist').text
+  song = m.at_css('.song').text
+
+  artist_song = Song.new(artist, song)
 end
 
 token = ENV['TELEGRAM_BOT_API_KEY']
@@ -23,10 +34,9 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} now play #{artist_song}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
   end
 end
-
