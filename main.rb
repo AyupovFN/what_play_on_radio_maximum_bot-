a7749a2b931e4463c2d58704582792adccfa14d0
AyupovFN <kapterka@bk.ru>
2020-06-10 23:05:59 +0300

    Add new method take_song

--- main.rb
+++ main.rb
@@ -4,6 +4,20 @@ require 'open-uri'
 require 'oga'
 require 'pry'
 
+def take_song
+  url = 'https://maximum.ru/'
+  html = open(url)
+  doc = Oga.parse_html(html)
+
+  doc.css('.track-animation').each do |m|
+    artist = m.at_css('.artist').text
+    song = m.at_css('.song').text
+
+    @artist_song = Song.new(artist, song)
+  end
+  @artist_song
+end
+
 class Song
   attr_reader :artist, :song
 
@@ -17,24 +31,13 @@ class Song
   end
 end
 
-URL = 'https://maximum.ru/'
-html = open(URL)
-doc = Oga.parse_html(html)
-
-doc.css('.track-animation').each do |m|
-  artist = m.at_css('.artist').text
-  song = m.at_css('.song').text
-
-  @artist_song = Song.new(artist, song)
-end
-
 token = ENV['TELEGRAM_BOT_API_KEY']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} now play #{@artist_song}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} now play #{take_song}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
